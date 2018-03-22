1b8e48504ea19da60aeb29f642be253a051c1dec
Shannon Doepke <shannondoepke@Shannons-MacBook-Air.local>
2018-03-22 13:24:07 -0400

    Update with initial styling and formatting

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//Create object with questions and answers array
+
+var qAndA = [
+    {"Which of these is not one of Chicago's nicknames?": "The Windy City", "The City of Big Shoulders", "The Little Apple", "The Second City"},
+    {"The Chicago Cubs won the World Series in 2016. Before then, how many years had it been since their last World Series win?": "108", "71", "56","122"},
+    {"For what holiday is the Chicago River dyed every year?": "Valentines Day", "St. Patrick's Day", "Christmas", "Fourth of July"},
+    {"Which of these was invented in Chicago?":"The Zipper", "The Twinkie", "The Ferris Wheel", "All of the Above"},
+    {"How many states are visible from the top of Chicago's Sears Tower?": "Six", "Five", "Four", "Three"},
+    {"The Chicago River is the only River in the world that _______.": "Flows backwards", "Divides a city equally", "Is naturally green", "Contains catfish"}
+];
